d6ae2098328932cc1fd16f3c9bd2875db88cce6f
Wiktor Waj <wiktor.waj@gmail.com>
2022-05-23 20:28:37 +0200

    Revert "Merge branch 'development'"
    This reverts commit 6afc2d40b212abd6a86db3a927fce55db108f9f5, reversing
    changes made to 326e0d195920b0848c649e84967068c713ddb3e0.

--- frontend/src/components/allpage.js
+++ frontend/src/components/allpage.js
@@ -1,10 +1,8 @@
 import React from "react";
-import Tmp from "./tmp";
 import MainMenu from "./mainmenu";
-import { BrowserRouter as Router, Route, Routes as Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes as Switch, useLocation } from 'react-router-dom';
 import { TabContext } from '@mui/lab';
 import AlgPreview from "./algpreview";
-
 
 export default class AllPage extends React.Component {
     getMainMenu(id) {
@@ -25,24 +23,11 @@ export default class AllPage extends React.Component {
 
     render() {
         return <>
-        <TabContext value={this.props.tabID}>
-            <MainMenu oggleDark={this.props.toggleDark} settoggleDark={this.props.settoggleDark}
-                    tabID={this.props.tabID} settabID={this.props.settabID}
-                    algActive={this.props.algActive} setalgActive={this.props.setalgActive} />
             <Switch>
-                <Route  exact path='/' element={<Tmp
-                    toggleDark={this.props.toggleDark} settoggleDark={this.props.settoggleDark}
-                    tabID={this.props.tabID} settabID={this.props.settabID}
-                    algActive={this.props.algActive} setalgActive={this.props.setalgActive}
-                />}></Route> 
-                <Route exact path='/moduly' element={<AlgPreview algActive={this.props.algActive} setalgActive={this.props.setalgActive} />}></Route>
-                 <Route exact path='/info' element={<Tmp
-                    toggleDark={this.props.toggleDark} settoggleDark={this.props.settoggleDark}
-                    tabID={this.props.tabID} settabID={this.props.settabID}
-                    algActive={this.props.algActive} setalgActive={this.props.setalgActive}
-                />}></Route>
+                <Route exact path='/' element={this.getMainMenu(0)}></Route>
+                <Route exact path='/moduly' element={this.getMainMenu(1)}></Route>
+                <Route exact path='/info' element={this.getMainMenu(2)}></Route>
             </Switch>
-            </TabContext>
         </>
     }
 }
