e75443f4d68ef7781017880522c0aa1b9da9be01
Sarneusz <mateuszgruszkiewicz@gmail.com>
2022-06-16 18:16:33 +0200

    testy tesciki

---
+++ frontend/src/components/Main/Main.test.js
@@ -0,0 +1,29 @@
+import {fireEvent, render, screen} from '@testing-library/react';
+import React from 'react';
+import {Main} from "./Main";
+
+
+
+describe('testy Main.js' , () => {
+
+    it('Render Main.js',() => {
+        render(<Main />);
+    });
+
+    it('Create main div',() => {
+        render(<Main />);
+        const element = screen.getByText('main');
+        expect(element).toBeInTheDocument();
+    });
+
+    it('main div in the doom',() => {
+        render(<Main />);
+        const element = screen.getByText('main');
+        expect(element).toBeInTheDOM();
+    });
+
+
+});
+
+
+
